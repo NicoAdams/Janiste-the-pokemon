0e7991624041056cccc0f7923b5c69f068e227d3
Steven Kalt <kalt.steven@gmail.com>
2020-06-22 20:00:55 -0400

    feat: add state management / event bus

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log("General kenobi!!!");
+import {store} from "./state"
+const unbind = store.on('changeDirection', (store, direction) => console.log({direction}))
+store.dispatch('changeDirection', 'up')
+console.log({store})
+unbind()
---
+++ src/state.js
@@ -0,0 +1,18 @@
+// docs at https://www.npmjs.com/package/storeon
+import { createStoreon } from 'storeon'
+import { storeonDevtools } from 'storeon/devtools';
+
+// tracks direction
+const currentDirection = (store) =>  {
+  store.on('@init', () => ({direction: null}))
+  /**
+   * @param {string} direction "up" | "down" | "left" | "right" | null
+   */
+  const changeDirection = (_, direction) => ({direction})
+  store.on('changeDirection', changeDirection)
+}
+
+export const store = createStoreon([currentDirection, storeonDevtools]) 
+// you'd `import {store} from "./state.js"; const unbindEvent = store.on("  ")`
+// working but commented example:
+store.dispatch('changeDirection', 'up')

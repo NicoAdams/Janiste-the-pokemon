e1e9d90c7721e4f4bd5f4c4a725797634296eae2
Jackson Henry <jacksonhenry3@gmail.com>
2020-07-25 22:37:15 -0400

    fractals basics and starting state structure

--- src/index.js
+++ src/index.js
@@ -1,18 +1,58 @@
 import "./sound";
-import { store } from "./state";
+// import { store } from "./state";
 import {select} from "d3"
 import "./style.scss"
 const gameDiv = document.getElementById("app")
-const gameViewport = document.createElement('svg')
-gameDiv.appendChild(gameViewport)
+const gameViewport = select("#app").append("svg")
 
-const square = () => select('rect').attr('height', 10).attr('width', 10).attr('x', 0).attr('y', 0)
-// setTimeout(() => {}, )
-console.log(square())
-const unbind = store.on("changeDirection", (store, direction) =>
-  console.log({ direction })
-);
+const drawLine = (line) => {gameViewport.append("line")
+								.attr("x1", line.x1)
+								.attr("y1", line.y1)
+								.attr("x2", line.x2)
+								.attr("y2", line.y2)
+								.attr("stroke-width",1)
+								.attr("stroke-linecap","round")
+								.attr("stroke", "black");
+							}
 
-store.dispatch("changeDirection", "up");
-console.log({ store });
-unbind();
+function line(origin, length, angle) {
+	this.origin = origin
+
+	this.length = length
+	this.angle = angle
+	this.x1 = origin.x
+	this.y1 = origin.y
+	this.x2 = origin.x+length*Math.cos(angle)
+	this.y2 = origin.y+length*Math.sin(angle)
+	this.terminus = {x:this.x2,y:this.y2}
+}
+
+
+
+let startX = 0
+let startY = 0
+let endX = 300
+let endY = 300
+let oldList = [new line({x:startX,y:startY},100,Math.PI/4)]
+const tick = () => {
+	let L = oldList.length
+	for (var i = L - 1; i >= 0; i--) {
+		let l = oldList[0]
+		let len = Math.sqrt((l.x2-l.x1)**2+(l.y2-l.y1)**2)/1.77
+		let angle = Math.atan2(l.y2-l.y1,l.x2-l.x1)
+		let r = Math.random()
+		oldList.push(new line({x:l.x2,y:l.y2},len,angle+Math.PI*r/5))
+		oldList.push(new line({x:l.x2,y:l.y2},len,angle-Math.PI*r/5))
+		oldList.shift()
+	}
+	for (var i = oldList.length - 1; i >= 0; i--) {
+		drawLine(oldList[i])
+	}
+	if (oldList.length>2048)
+	{
+		clearInterval(myVar);
+	}
+	
+
+}
+let myVar = setInterval(tick, 100);
--- src/state.js
+++ src/state.js
@@ -2,6 +2,13 @@
 import { createStoreon } from "storeon";
 import { storeonDevtools } from "storeon/devtools";
 
+const origin = {x:0,y:0},
+length = 100;
+
+const initialTheta = Math.PI/5;
+const initialLengthMultiplier = .8;
+
+
 // tracks direction
 const currentDirection = (store) => {
   store.on("@init", () => ({ direction: null }));
@@ -12,7 +19,46 @@ const currentDirection = (store) => {
   store.on("changeDirection", changeDirection);
 };
 
-export const store = createStoreon([currentDirection, storeonDevtools]);
+
+
+let angle = store => {
+  store.on('@init', () => ({ theta: initialTheta }))
+  store.on('setAngle', ({ newTtheta }) => ({ theta: newTtheta }))
+}
+
+let lengthMultiplier = store => {
+  store.on('@init', () => ({ l: initialLengthMultiplier }))
+  store.on('setLengthMultiplier', ({ newL }) => ({ l: newL }))
+}
+
+let leaves = store => {
+  store.on('@init', () => ({ leaves: [new line(origin,length, store.angle) }))
+  store.on('generateNewLeaves', ({}) => (
+  	let newLeaves = []
+  	for (var i = leaves.length - 1; i >= 0; i--) {
+  		branch = leaves[i]
+  		newLeaves.push(new line(branch.terminus,branch.length*lengthMultiplier,branch.angle+theta))
+  		newLeaves.push(new line(branch.terminus,branch.length*lengthMultiplier,branch.angle-theta))
+  }
+  leaves: newLeaves
+  )
+}
+
+let render = store => {
+  store.on('render', ({ newL }) => ({ for (var i = leaves.length - 1; i >= 0; i--) {
+  	leaf = leaves[i]
+  	drawLine(leaf)
+  } }))
+}
+
+let tick = store => {
+  store.on('tick', ({ newL }) => (store.generateNewLeaves(); store.render()))
+}
+
+
+ 
+
+export const store = createStoreon([angle,lengthMultiplier, storeonDevtools]);
 // you'd `import {store} from "./state.js"; const unbindEvent = store.on("  ")`
 // working but commented example:
-store.dispatch("changeDirection", "up");
+// store.dispatch("changeDirection", "up");
